2f3cfd3aca750072c5c6756968e58c47cf015941
acs0209 <annjae4676@naver.com>
2024-11-14 13:15:38 +0900

    Init project

---
+++ filmeet/src/main/java/com/ureca/filmeet/FilmeetApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.filmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeetApplication.class, args);
+	}
+
+}
