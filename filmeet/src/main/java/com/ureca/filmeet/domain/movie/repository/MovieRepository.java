4ead5763420e72549831a37e73fdf83233788a80
acs0209 <annjae4676@naver.com>
2024-11-19 15:11:48 +0900

    Refactor: 사용자 추천 영화 select SQL 조건 추가

--- filmeet/src/main/java/com/ureca/filmeet/domain/movie/repository/MovieRepository.java
+++ filmeet/src/main/java/com/ureca/filmeet/domain/movie/repository/MovieRepository.java
@@ -27,18 +27,20 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     )
     List<Movie> findMoviesWithStarRatingAndLikes();
 
-    @Query("SELECT DISTINCT m " +
-            "FROM MovieGenre mg " +
-            "LEFT JOIN mg.movie m " +
+    @Query("SELECT m " +
+            "FROM Movie m " +
+            "JOIN m.movieGenres mg " +
             "LEFT JOIN Review r ON r.movie.id = m.id AND r.member.memberId = :memberId " +
             "LEFT JOIN MovieLikes ml ON ml.movie.id = m.id AND ml.member.memberId = :memberId " +
             "LEFT JOIN Collection c ON c.movie.id = m.id AND c.member.memberId = :memberId " +
             "WHERE mg.genre.id IN :genreIds " +
             "AND r.id IS NULL " +
             "AND ml.id IS NULL " +
-            "AND c.id IS NULL")
+            "AND c.id IS NULL " +
+            "AND m.id NOT IN :top10MovieIds")
     List<Movie> findMoviesByPreferredGenresAndNotInteracted(
             @Param("genreIds") List<Long> genreIds,
-            @Param("memberId") Long memberId
+            @Param("memberId") Long memberId,
+            @Param("top10MovieIds") List<Long> top10MovieIds
     );
 }
--- filmeet/src/main/java/com/ureca/filmeet/domain/movie/service/query/MovieRecommendationQueryService.java
+++ filmeet/src/main/java/com/ureca/filmeet/domain/movie/service/query/MovieRecommendationQueryService.java
@@ -21,14 +21,34 @@ public class MovieRecommendationQueryService {
     private final GenreScoreRepository genreScoreRepository;
 
     public List<RecommendationMoviesResponse> getMoviesRecommendation(Long memberId) {
+        List<Long> top10MovieIds = getTop10MovieIds();
         List<Long> genreIds = genreScoreRepository.findTop10GenreIdsByMemberId(memberId);
-        List<Movie> preferredMovies = movieRepository.findMoviesByPreferredGenresAndNotInteracted(genreIds, memberId);
+        List<Movie> preferredMovies = movieRepository.findMoviesByPreferredGenresAndNotInteracted(genreIds, memberId,
+                top10MovieIds);
         Map<Movie, Double> movieScores = movieScoreService.calculateMovieScores(preferredMovies);
+
         return movieScores.entrySet()
                 .stream()
                 .sorted(Map.Entry.<Movie, Double>comparingByValue().reversed())
                 .limit(10)
                 .map(entry -> RecommendationMoviesResponse.of(entry.getKey()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Filmeet TOP 10 영화 ID 리스트를 반환합니다.
+     *
+     * @return TOP 10 영화 ID 리스트
+     */
+    private List<Long> getTop10MovieIds() {
+        List<Movie> movies = movieRepository.findMoviesWithStarRatingAndLikes();
+        Map<Movie, Double> movieScores = movieScoreService.calculateMovieScores(movies);
+
+        return movieScores.entrySet()
+                .stream()
+                .sorted(Map.Entry.<Movie, Double>comparingByValue().reversed())
+                .limit(10)
+                .map(entry -> entry.getKey().getId())
                 .collect(Collectors.toList());
     }
 }
