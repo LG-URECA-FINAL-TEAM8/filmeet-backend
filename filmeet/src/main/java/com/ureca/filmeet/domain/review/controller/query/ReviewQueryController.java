36f00a5be21d43d3eb3029c0cde51d5c2b7af204
acs0209 <annjae4676@naver.com>
2024-11-28 11:50:11 +0900

    refactor: 지금 뜨는 리뷰 조회 API @AuthenticationPrincipal 적용

--- filmeet/src/main/java/com/ureca/filmeet/domain/review/controller/query/ReviewQueryController.java
+++ filmeet/src/main/java/com/ureca/filmeet/domain/review/controller/query/ReviewQueryController.java
@@ -5,12 +5,14 @@ import com.ureca.filmeet.domain.review.dto.response.GetMovieReviewsResponse;
 import com.ureca.filmeet.domain.review.dto.response.trending.ReviewTrendingResponse;
 import com.ureca.filmeet.domain.review.service.query.ReviewQueryService;
 import com.ureca.filmeet.domain.review.service.query.ReviewTrendingQueryService;
+import com.ureca.filmeet.domain.user.entity.User;
 import com.ureca.filmeet.global.common.dto.ApiResponse;
 import com.ureca.filmeet.global.common.dto.SliceResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,17 +46,19 @@ public class ReviewQueryController {
         return ApiResponse.ok(movieReviewDetail);
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/users")
     public ResponseEntity<ApiResponse<SliceResponseDto<ReviewTrendingResponse>>> getTrendingReviews(
             @RequestParam("filter") String filter,
-            @PathVariable("userId") Long userId,
+            @AuthenticationPrincipal User user,
             Pageable pageable
     ) {
         if (filter.equals("recent_reviews")) {
-            Slice<ReviewTrendingResponse> recentReviews = reviewTrendingQueryService.getRecentReviews(userId, pageable);
+            Slice<ReviewTrendingResponse> recentReviews = reviewTrendingQueryService.getRecentReviews(user.getId(),
+                    pageable);
             return ApiResponse.ok(SliceResponseDto.of(recentReviews));
         }
-        Slice<ReviewTrendingResponse> trendingReviews = reviewTrendingQueryService.getTrendingReviews(userId, pageable);
+        Slice<ReviewTrendingResponse> trendingReviews = reviewTrendingQueryService.getTrendingReviews(user.getId(),
+                pageable);
         return ApiResponse.ok(SliceResponseDto.of(trendingReviews));
     }
 }
