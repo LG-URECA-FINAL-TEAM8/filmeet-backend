c94fb3e406d72998a0cda05ae9d2524ea4b92222
acs0209 <annjae4676@naver.com>
2024-12-02 10:32:49 +0900

    refactor: 예외 클래스 상속 수정

--- filmeet/src/main/java/com/ureca/filmeet/domain/collection/exception/CollectionUserNotFoundException.java
+++ filmeet/src/main/java/com/ureca/filmeet/domain/collection/exception/CollectionUserNotFoundException.java
@@ -1,9 +1,8 @@
 package com.ureca.filmeet.domain.collection.exception;
 
-import com.ureca.filmeet.global.exception.GlobalException;
 import com.ureca.filmeet.global.exception.code.ResponseCode;
 
-public class CollectionUserNotFoundException extends GlobalException {
+public class CollectionUserNotFoundException extends CollectionException {
 
     public CollectionUserNotFoundException() {
         super(ResponseCode.COLLECTION_USER_NOT_FOUND);
