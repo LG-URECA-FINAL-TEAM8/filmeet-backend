98016c2b91fe2002658ec113dd380b86504b883b
acs0209 <annjae4676@naver.com>
2024-12-14 23:05:19 +0900

    refactor: 개인 맞춤형 추천 영화 리스트 조회 API에 TOP 10 영화 리스트 필터링 캐시 적용

--- filmeet/src/main/java/com/ureca/filmeet/domain/movie/service/query/MovieRecommendationQueryService.java
+++ filmeet/src/main/java/com/ureca/filmeet/domain/movie/service/query/MovieRecommendationQueryService.java
@@ -1,17 +1,21 @@
 package com.ureca.filmeet.domain.movie.service.query;
 
 import com.ureca.filmeet.domain.genre.repository.GenreScoreRepository;
+import com.ureca.filmeet.domain.movie.dto.response.MoviesRankingsResponse;
 import com.ureca.filmeet.domain.movie.dto.response.RecommendationMoviesResponse;
 import com.ureca.filmeet.domain.movie.entity.Movie;
 import com.ureca.filmeet.domain.movie.repository.MovieRepository;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -20,6 +24,7 @@ public class MovieRecommendationQueryService {
     private final MovieRepository movieRepository;
     private final MovieScoreService movieScoreService;
     private final GenreScoreRepository genreScoreRepository;
+    private final MoviesRankingsRedisQueryService moviesRankingsRedisQueryService;
 
     public List<RecommendationMoviesResponse> getMoviesRecommendation(Long userId, Pageable pageable) {
         List<Long> top10MovieIds = getTop10MovieIds();
@@ -46,14 +51,38 @@ public class MovieRecommendationQueryService {
      * @return TOP 10 영화 ID 리스트
      */
     private List<Long> getTop10MovieIds() {
+        List<Long> top10MovieIds = getTop10MovieIdsFromRedis();
+        if (top10MovieIds != null && top10MovieIds.size() == 10) {
+            log.info("레디스에 TOP 10 영화 데이터가 있습니다. 영화 ID: {}", top10MovieIds);
+            return top10MovieIds;
+        }
+        log.info("레디스에 TOP 10 영화 데이터가 없어 DB로부터 데이터를 가져옵니다.");
+        return getTop10MovieIdsFromDbAndSaveToRedis();
+    }
+
+    private List<Long> getTop10MovieIdsFromRedis() {
+        return moviesRankingsRedisQueryService.getTop10MovieIds();
+    }
+
+    private List<Long> getTop10MovieIdsFromDbAndSaveToRedis() {
         List<Movie> movies = movieRepository.findMoviesWithStarRatingAndLikesUnion();
         Map<Movie, Double> movieScores = movieScoreService.calculateMovieScores(movies);
-
+        saveMoviesRankings(movieScores);
         return movieScores.entrySet()
                 .stream()
                 .sorted(Map.Entry.<Movie, Double>comparingByValue().reversed())
                 .limit(10)
                 .map(entry -> entry.getKey().getId())
                 .collect(Collectors.toList());
+    }
+
+    private void saveMoviesRankings(Map<Movie, Double> movieScores) {
+        List<MoviesRankingsResponse> moviesRankingsResponse = movieScores.entrySet()
+                .stream()
+                .sorted(Entry.<Movie, Double>comparingByValue().reversed())
+                .limit(10)
+                .map(entry -> MoviesRankingsResponse.of(entry.getKey()))
+                .toList();
+        moviesRankingsRedisQueryService.saveMoviesRankings(moviesRankingsResponse);
     }
 }
