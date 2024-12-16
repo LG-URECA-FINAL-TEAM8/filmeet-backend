245637629618babc6c757d412d5975ffebbf7645
acs0209 <annjae4676@naver.com>
2024-12-16 09:35:53 +0900

    test: CollectionCommentCommandServiceTest 테스트 코드 수정

--- filmeet/src/test/java/com/ureca/filmeet/domain/collection/service/command/CollectionCommentCommandServiceTest.java
+++ filmeet/src/test/java/com/ureca/filmeet/domain/collection/service/command/CollectionCommentCommandServiceTest.java
@@ -1,12 +1,10 @@
 package com.ureca.filmeet.domain.collection.service.command;
 
 import static com.ureca.filmeet.global.util.TestUtils.createCollection;
+import static com.ureca.filmeet.global.util.TestUtils.createCollectionComment;
+import static com.ureca.filmeet.global.util.TestUtils.createUser;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import com.ureca.filmeet.domain.collection.dto.request.CollectionCommentCreateRequest;
 import com.ureca.filmeet.domain.collection.dto.request.CollectionCommentDeleteRequest;
@@ -18,59 +16,63 @@ import com.ureca.filmeet.domain.collection.exception.CollectionNotFoundException
 import com.ureca.filmeet.domain.collection.exception.CollectionUserNotFoundException;
 import com.ureca.filmeet.domain.collection.repository.CollectionCommentRepository;
 import com.ureca.filmeet.domain.collection.repository.CollectionRepository;
+import com.ureca.filmeet.domain.user.entity.Provider;
+import com.ureca.filmeet.domain.user.entity.Role;
 import com.ureca.filmeet.domain.user.entity.User;
 import com.ureca.filmeet.domain.user.repository.UserRepository;
-import com.ureca.filmeet.global.util.string.BadWordService;
 import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ActiveProfiles("local")
 class CollectionCommentCommandServiceTest {
 
-
-    @InjectMocks
+    @Autowired
     private CollectionCommentCommandService collectionCommentCommandService;
 
-    @Mock
+    @Autowired
     private UserRepository userRepository;
 
-    @Mock
+    @Autowired
     private CollectionRepository collectionRepository;
 
-    @Mock
+    @Autowired
     private CollectionCommentRepository collectionCommentRepository;
 
-    @Mock
-    private BadWordService badWordService;
+    @AfterEach
+    void tearDown() {
+        collectionCommentRepository.deleteAllInBatch();
+        collectionRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
 
-    @DisplayName("사용자가 컬렉션에 댓글을 성공적으로 작성하고 욕성 필터링이 동작한다.")
+    @DisplayName("사용자가 컬렉션에 댓글을 성공적으로 작성한다.")
     @Test
     void createCollectionComment_whenValidRequest_savesComment() {
         // given
-        String commentContent = "This is a comment";
-        String maskedCommentContent = "This is a ****";
-        User user = mock(User.class);
-        Collection collection = mock(Collection.class);
-        CollectionComment mockComment = new CollectionComment(maskedCommentContent, user, collection);
-        CollectionCommentCreateRequest request = new CollectionCommentCreateRequest(collection.getId(), commentContent);
+        User user = createUser("username", "securePassword", Role.ROLE_ADULT_USER, Provider.NAVER, "닉네임",
+                "https://example.com/profile.jpg");
+        Collection collection = createCollection("컬렉션 제목", "컬렉션 내용", user);
 
         // when
-        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
-        when(collectionRepository.findById(collection.getId())).thenReturn(Optional.of(collection));
-        when(badWordService.maskText(commentContent)).thenReturn(maskedCommentContent);
-        when(collectionCommentRepository.save(any(CollectionComment.class))).thenReturn(mockComment);
+        userRepository.save(user);
+        collectionRepository.save(collection);
+        CollectionCommentCreateRequest request = new CollectionCommentCreateRequest(collection.getId(), "댓글 내용");
         Long commentId = collectionCommentCommandService.createCollectionComment(request, user.getId());
+        Optional<CollectionComment> savedComment = collectionCommentRepository.findById(commentId);
 
         // then
-        verify(badWordService).maskText(commentContent);
-        verify(collectionCommentRepository).save(any(CollectionComment.class));
-        assertThat(commentId).isEqualTo(mockComment.getId());
-        assertThat(mockComment.getContent()).isEqualTo(maskedCommentContent);
+        assertThat(savedComment)
+                .isPresent()
+                .get()
+                .extracting("id", "content", "user.id", "collection.id", "collection.commentCounts")
+                .contains(commentId, request.commentContent(), user.getId(), collection.getId(), 1
+                );
     }
 
     @Test
@@ -92,13 +94,12 @@ class CollectionCommentCommandServiceTest {
     @DisplayName("존재하지 않는 컬렉션에 댓글을 작성하려고 하면 CollectionNotFoundException 이 발생한다.")
     void createCollectionComment_whenCollectionNotFound_throwsException() {
         // given
-        Long nonExistentCollectionId = 999L;
-        User user = mock(User.class);
-        CollectionCommentCreateRequest request = new CollectionCommentCreateRequest(nonExistentCollectionId, "댓글 내용");
+        User user = createUser("username", "securePassword", Role.ROLE_ADULT_USER, Provider.NAVER, "닉네임",
+                "https://example.com/profile.jpg");
+        CollectionCommentCreateRequest request = new CollectionCommentCreateRequest(999L, "댓글 내용");
 
         // when
-        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
-        when(collectionRepository.findById(nonExistentCollectionId)).thenReturn(Optional.empty()); // 컬렉션이 없을 경우
+        userRepository.save(user);
 
         // then
         assertThatThrownBy(() -> collectionCommentCommandService.createCollectionComment(request, user.getId()))
@@ -107,26 +108,28 @@ class CollectionCommentCommandServiceTest {
 
     @Test
     @DisplayName("사용자가 자신의 댓글을 성공적으로 수정한다.")
-    void modifyCollectionComment_shouldModifyCommentSuccessfully() {
+    void modifyCollectionComment_whenValidRequest_updatesComment() {
         // given
-        Long userId = 1L;
-        Long commentId = 2L;
-        String modifiedComment = "Modified Comment";
-        String maskedComment = "**** Comment";
-
-        CollectionCommentModifyRequest request = new CollectionCommentModifyRequest(commentId, modifiedComment);
-        CollectionComment collectionComment = mock(CollectionComment.class);
+        User user = createUser("username", "securePassword", Role.ROLE_ADULT_USER, Provider.NAVER, "닉네임",
+                "https://example.com/profile.jpg");
+        Collection collection = createCollection("컬렉션 제목", "컬렉션 내용", user);
+        CollectionComment comment = createCollectionComment("댓글 내용", user, collection);
 
         // when
-        when(collectionCommentRepository.findCollectionCommentWrittenUserBy(userId, commentId))
-                .thenReturn(Optional.of(collectionComment));
-        when(badWordService.maskText(modifiedComment)).thenReturn(maskedComment);
-        collectionCommentCommandService.modifyCollectionComment(request, userId);
+        userRepository.save(user);
+        collectionRepository.save(collection);
+        collectionCommentRepository.save(comment);
+        CollectionCommentModifyRequest request = new CollectionCommentModifyRequest(comment.getId(), "수정된 댓글 내용");
+        Long updatedCommentId = collectionCommentCommandService.modifyCollectionComment(request, user.getId());
+        Optional<CollectionComment> updatedComment = collectionCommentRepository.findById(updatedCommentId);
 
         // then
-        verify(collectionCommentRepository).findCollectionCommentWrittenUserBy(userId, commentId);
-        verify(badWordService).maskText(modifiedComment);
-        verify(collectionComment).modifyCollectionComment(maskedComment);
+        assertThat(updatedComment)
+                .isPresent()
+                .get()
+                .extracting("id", "content", "user.id", "collection.id", "collection.commentCounts")
+                .contains(updatedComment.get().getId(), request.commentContent(), user.getId(), collection.getId(), 0
+                );
     }
 
     @Test
@@ -142,27 +145,26 @@ class CollectionCommentCommandServiceTest {
 
     @Test
     @DisplayName("사용자가 자신의 댓글을 성공적으로 삭제한다.")
-    void deleteCollectionComment_shouldDeleteCommentSuccessfully() {
+    void deleteCollectionComment_whenValidRequest_deletesComment() {
         // given
-        Long userId = 1L;
-        Long commentId = 2L;
-        Long collectionId = 3L;
-
-        CollectionCommentDeleteRequest request = new CollectionCommentDeleteRequest(collectionId, commentId);
-
-        CollectionComment collectionComment = mock(CollectionComment.class);
-        Collection collection = mock(Collection.class);
+        User user = createUser("username", "securePassword", Role.ROLE_ADULT_USER, Provider.NAVER, "닉네임",
+                "https://example.com/profile.jpg");
+        Collection collection = createCollection("컬렉션 제목", "컬렉션 내용", user);
+        CollectionComment comment = createCollectionComment("댓글 내용", user, collection);
 
         // when
-        when(collectionCommentRepository.findCollectionCommentWithCollectionBy(userId, commentId))
-                .thenReturn(Optional.of(collectionComment));
-        when(collectionComment.getCollection()).thenReturn(collection);
-        collectionCommentCommandService.deleteCollectionComment(request, userId);
+        userRepository.save(user);
+        collectionRepository.save(collection);
+        collectionCommentRepository.save(comment);
+        CollectionCommentDeleteRequest request = new CollectionCommentDeleteRequest(collection.getId(),
+                comment.getId());
+        collectionCommentCommandService.deleteCollectionComment(request, user.getId());
+        Optional<CollectionComment> deletedComment = collectionCommentRepository.findById(comment.getId());
 
         // then
-        verify(collectionCommentRepository).findCollectionCommentWithCollectionBy(userId, commentId);
-        verify(collectionComment).delete();
-        verify(collection).decrementCommentCounts();
+        assertThat(deletedComment).isPresent();
+        assertThat(deletedComment.get().getIsDeleted()).isTrue();
+        assertThat(collection.getCommentCounts()).isEqualTo(0);
     }
 
     @Test
