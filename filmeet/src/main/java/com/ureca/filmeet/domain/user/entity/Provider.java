2ee53681e58537002127248e7cf772772d567a1e
eastsage <dpwk8@naver.com>
2024-12-17 16:23:22 +0900

    feat: 카카오 로그인 추가

--- filmeet/src/main/java/com/ureca/filmeet/domain/user/entity/Provider.java
+++ filmeet/src/main/java/com/ureca/filmeet/domain/user/entity/Provider.java
@@ -9,14 +9,24 @@ import java.util.Optional;
 @Getter
 @AllArgsConstructor
 public enum Provider {
-    //OIDC Provider
-    GOOGLE("google", true),
+    // OIDC Providers
+    GOOGLE("google", true, "https://accounts.google.com", "name"),
+    KAKAO("kakao", true, "https://kauth.kakao.com", "nickname"),
 
-    //OAuth2 Provider
-    NAVER("naver", false);
+    // OAuth2 Providers
+    NAVER("naver", false, "https://nid.naver.com", "name");
 
-    private final String name;
-    private final boolean isOidcProvider;
+    private final String name;        // Provider 이름
+    private final boolean isOidc;     // OIDC 여부
+    private final String issuer;      // iss 값
+    private final String nameKey;     // 사용자 이름 키
+
+    public static Provider fromIssuer(String issuer) {
+        return Arrays.stream(values())
+                .filter(provider -> issuer.contains(provider.getIssuer()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No matching provider for issuer: " + issuer));
+    }
 
     public static Optional<Provider> fromName(String name) {
         return Arrays.stream(values())
@@ -24,3 +34,4 @@ public enum Provider {
                 .findFirst();
     }
 }
+
