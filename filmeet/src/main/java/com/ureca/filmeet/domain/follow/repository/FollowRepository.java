895d49a5be543b7af07d162ce2b98e2ba3d035f9
jun10920 <abide0206@naver.com>
2024-12-17 10:31:58 +0900

    fix : 팔로우 api n+1 문제 해결

--- filmeet/src/main/java/com/ureca/filmeet/domain/follow/repository/FollowRepository.java
+++ filmeet/src/main/java/com/ureca/filmeet/domain/follow/repository/FollowRepository.java
@@ -2,9 +2,11 @@ package com.ureca.filmeet.domain.follow.repository;
 
 import com.ureca.filmeet.domain.follow.entity.Follow;
 import com.ureca.filmeet.domain.user.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,10 +16,15 @@ import java.util.Optional;
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     // 팔로우 관계 확인
-    boolean existsByFollowerAndFollowing(User follower, User following);
+    @Query("SELECT COUNT(f) > 0 FROM Follow f WHERE f.follower = :follower AND f.following = :following")
+    boolean existsByFollowerAndFollowing(@Param("follower") User follower, @Param("following") User following);
 
     // 팔로우 관계 조회
-    Optional<Follow> findByFollowerAndFollowing(User follower, User following);
+    @Query("SELECT f FROM Follow f " +
+            "JOIN FETCH f.follower " +
+            "JOIN FETCH f.following " +
+            "WHERE f.follower = :follower AND f.following = :following")
+    Optional<Follow> findByFollowerAndFollowing(@Param("follower") User follower, @Param("following") User following);
 
     // 모든 팔로우 조회
     Slice<Follow> findAllByFollower(User follower, Pageable pageable);
